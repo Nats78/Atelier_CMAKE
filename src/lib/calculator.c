73b6a3adeb76255b71ec7d1db2140050d5aeae3e
Nats78 <nathan.blr@orange.fr>
2024-11-12 14:52:31 +0100

    Update calculator.c

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -1,5 +1,21 @@
-#include "calculator.h"
+#include "./../../include/calculator.h"
 
-int square(int x) {
-    return x * x;
+double _add(double a, double b) {
+    return a + b;
+}
+
+double _sub(double a, double b) {
+    return a - b;
+}
+
+double _mul(double a, double b) {
+    return a * b;
+}
+
+double _div(double a, double b) {
+    return a / b;
+}
+
+double _car(double a) {
+    return a * a;
 }

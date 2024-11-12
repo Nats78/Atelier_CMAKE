f78b2d75e8b1dffb56a16fb8a32704d43d954068
Nats78 <nathan.blr@orange.fr>
2024-11-12 14:50:15 +0100

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,6 +1,5 @@
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
-
-int square(int x);
-
-#endif
+double _add(double a, double b);
+double _sub(double a, double b);
+double _mul(double a, double b);
+double _div(double a, double b);
+double _car(double a);

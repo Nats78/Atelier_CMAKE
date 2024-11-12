2c612a3e7d061096088d5505d2814135462a567c
Nats78 <nathan.blr@orange.fr>
2024-11-12 15:28:23 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,38 +4,38 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]) {
-    if (argc == 3) {
-        char *op = argv[1];
-        char *a = argv[2];
+    if (argc == 4 || (argc == 3 && strcmp(argv[1], "car") == 0)) {
+        char* op = argv[1];
         double r = 0;
-        char *endptr;
 
-        double num = strtod(a, &endptr);
-        if (*endptr != '\0') {
-            printf("Erreur: L'argument '%s' n'est pas un nombre valide.\n", a);
-            return 1;
+        if (strcmp(op, "add") == 0) {
+            r = _add(atof(argv[2]), atof(argv[3]));
+            printf("%lf", r);
         }
-
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
+        else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(argv[2]), atof(argv[3]));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(argv[2]), atof(argv[3]));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "div") == 0) {
+            r = _div(atof(argv[2]), atof(argv[3]));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "car") == 0 && argc == 3) {
+            r = _car(atof(argv[2]));
+            printf("%lf", r);
+        }
+        else {
+            printf("Erreur de paramètres");
+        }
+    }
+    else {
+        printf("Erreur de paramètres");
+    }
+    return 0;
 }
 
 		

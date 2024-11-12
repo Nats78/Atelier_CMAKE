7bf7e0a6f8d04f1068ef7c78b59fa61626374dde
Nats78 <nathan.blr@orange.fr>
2024-11-12 15:10:18 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,41 +3,33 @@
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]) {  // La signature correcte pour main
-    if (argc == 4 || (argc == 3 && strcmp(argv[1], "car") == 0)) {  // Vérifier les bons arguments
-        char *op = argv[1];  // Utilisation correcte du type char * pour les opérations
-        double r = 0;
-
-        if (strcmp(op, "add") == 0) {
-            r = add(atof(argv[2]), atof(argv[3]));  // Utilisation des fonctions sans underscore
-            printf("%.2lf\n", r);  // Formatage du résultat avec 2 décimales
-        }
-        else if (strcmp(op, "sub") == 0) {
-            r = sub(atof(argv[2]), atof(argv[3]));
-            printf("%.2lf\n", r);
-        }
-        else if (strcmp(op, "mul") == 0) {
-            r = mul(atof(argv[2]), atof(argv[3]));
-            printf("%.2lf\n", r);
-        }
-        else if (strcmp(op, "div") == 0) {
-            if (atof(argv[3]) == 0) {  // Vérification pour éviter la division par zéro
-                printf("Erreur : division par zéro\n");
-            } else {
-                r = div(atof(argv[2]), atof(argv[3]));
-                printf("%.2lf\n", r);
-            }
-        }
-        else if (strcmp(op, "car") == 0 && argc == 3) {  // Calcul du carré
-            r = car(atof(argv[2]));
-            printf("%.2lf\n", r);
-        }
-        else {
-            printf("Erreur de paramètres\n");
-        }
-    }
-    else {
-        printf("Erreur de paramètres\n");
-    }
-    return 0;
+int main(int argc, char *argv[]){
+	if (argc == 4)
+	{
+	char* op = argv[1];
+	char* a = argv[2];
+	char* b = argv[3];
+	double r = 0;
+	if (strcmp(op, "add") == 0) {
+		r = _add(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "sub") == 0) {
+		r = _sub(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "mul") == 0) {
+		r = _mul(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "div") == 0) {
+		r = _div(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else {printf("Erreur de parametres");}
+	}
+	else {printf("Erreur de parametres");}
+	return 0;
 }
+
+		
